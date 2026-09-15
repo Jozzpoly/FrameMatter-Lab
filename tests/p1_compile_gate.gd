917134c01aec8706dfa66c350e815f8215dc9e8e
Jozzpoly <100104127+Jozzpoly@users.noreply.github.com>
2026-09-15 13:34:21 +0200

    Compile canonical P1 startup gate

--- tests/p1_compile_gate.gd
+++ tests/p1_compile_gate.gd
@@ -37,6 +37,7 @@ const SCRIPT_PATHS: PackedStringArray = [
 	"res://tests/p1_storage_actor_context_challenger.gd",
 	"res://tests/p1_finite_space_control_probe.gd",
 	"res://tests/p1_integrated_causal_loop_probe.gd",
+	"res://tests/p1_canonical_startup_smoke.gd",
 ]
 
 var _failures: Array[String] = []
